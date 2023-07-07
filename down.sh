37a3f8a202a127c24bc52b0d4db507928e9616c9
Giedo Stolk <giedo.stolk@ns.nl>
2023-07-07 14:20:53 +0200

    Updates

---
+++ down.sh
@@ -0,0 +1,1 @@
+ssh giedo@192.168.1.162 'cd kubernetes && vagrant destroy -f' 
---
+++ up.sh
@@ -0,0 +1,1 @@
+ssh giedo@192.168.1.162 'rm -rf kubernetes ; git clone https://github.com/stolkie78/vagrant-kubeadm-kubernetes kubernetes && cd kubernetes && vagrant up' 
